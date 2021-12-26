7527d0f654e0aeddb609c3c1d172fbab3a91228e
Alexey Korneev <korneevkot@yandex.ru>
2021-12-26 18:48:58 +0300

    add impl userDaoJDBC

--- src/testforme/Test1.java
+++ src/testforme/Test1.java
@@ -2,17 +2,27 @@ import java.sql.*;
 
 public class Test1 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        String url = "jdbc:mysql://localhost:3306/world";
-        String userName = "root";
-        String pass = "kotovsk";
+        String url = "jdbc:mysql://localhost:3306/mydb";
+        String userName = "test";
+        String pass = "secret";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         Connection connection = DriverManager.getConnection(url,userName,pass);
         connection.setAutoCommit(false);
         Statement statement = connection.createStatement();
-        String sql = "SELECT * FROM city";
+        String deleteTable = "drop table userTable";
+        String newSqlTable = "Create table userTable " +
+                "(id integer not null, " +
+                " name Varchar(50), " +
+                " lastname Varchar(50), " +
+                " age integer not null, " +
+                "primary key (id))";
+        String sql = "SELECT * FROM userTable";
+        statement.executeUpdate(deleteTable);
+        statement.executeUpdate(newSqlTable);
         ResultSet resultSet = statement.executeQuery(sql);
+
         while (resultSet.next()){
             int id = resultSet.getInt("ID");
             System.out.println("id " + id);
